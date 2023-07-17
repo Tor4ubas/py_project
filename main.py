ae74a5abfafeb513305a5fd1091b10b9f23f3742
Tor4ubas <alex1029@yandex.ru>
2023-07-17 19:43:30 +0300

    initial

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from funcs import summ_numbers, difference_numbers
 result = summ_numbers(5, 4)
 print(result)
 
-result_1 = difference_numbers(5, 4)
+result_1 = difference_numbers(6, 3)
 print(result_1)
+
 
